2ba4531735bb01a9418fcecb28ca6dd86bfe71ad
Shakshi3104 <51497924+Shakshi3104@users.noreply.github.com>
2023-02-26 10:19:30 +0900

    Implemented live info loader

--- utils/utils.py
+++ utils/utils.py
@@ -4,6 +4,9 @@ import streamlit as st
 
 from pathlib import Path
 
+import re
+import unicodedata
+
 
 @st.cache
 def load_all_songs(dirpath):
@@ -98,6 +101,71 @@ def load_album_sales(dirpath: str):
     return album_sales
 
 
+# 結合文字を使用した濁点や半濁点を直前の仮名と結合させる
+# https://qiita.com/gretchi/items/13c0825282415e2e360d
+def join_diacritic(text, mode="NFC"):
+    """
+    基底文字と濁点・半濁点を結合
+    """
+    # str -> bytes
+    bytes_text = text.encode()
+
+    # 濁点Unicode結合文字置換
+    bytes_text = re.sub(b"\xe3\x82\x9b", b'\xe3\x82\x99', bytes_text)
+    bytes_text = re.sub(b"\xef\xbe\x9e", b'\xe3\x82\x99', bytes_text)
+
+    # 半濁点Unicode結合文字置換
+    bytes_text = re.sub(b"\xe3\x82\x9c", b'\xe3\x82\x9a', bytes_text)
+    bytes_text = re.sub(b"\xef\xbe\x9f", b'\xe3\x82\x9a', bytes_text)
+
+    # bytet -> str
+    text = bytes_text.decode()
+
+    # 正規化
+    text = unicodedata.normalize(mode, text)
+
+    return text
+
+@st.cache
+def load_live_info(dirpath):
+    dirpath = Path(dirpath)
+    dirpath_dvd = dirpath / "DVD"
+
+    live_songs = pd.DataFrame()
+
+    for filepath in dirpath_dvd.glob("*.csv"):
+        # ライブ名
+        live_name = join_diacritic(filepath.name.replace("-表1.csv", ""))
+
+        live = pd.read_csv(filepath)
+        live["コンサート名"] = live_name
+
+        live_songs = pd.concat([live_songs, live], ignore_index=True)
+
+    # カウントする
+    counted_live_songs = live_songs.groupby(by="曲名").size().sort_values(ascending=False).to_frame().reset_index()
+    counted_live_songs = counted_live_songs.rename(columns={0: "収録回数"})
+
+    # コンサートの情報
+    live_info = pd.read_csv(dirpath / "live-info.csv")
+
+    # コンサート情報とマージする
+    live_songs = pd.merge(live_info, live_songs, left_on="名前", right_on="コンサート名").drop(columns="名前")
+
+    # 曲名とコンサート名のクロス集計表
+    songs_lives_cross = pd.crosstab(live_songs["曲名"], live_songs["略称"])
+    songs_lives_cross = songs_lives_cross[list(live_info["略称"])]
+
+    return songs_lives_cross, counted_live_songs, live_info
+
+
 if __name__ == "__main__":
     dirpath = "../data/"
     all_, writes, info = load_all_songs(dirpath)
+
+    songs_lives_cross, counted_live_songs, live_info = load_live_info(dirpath)
+
+    all_song_ = pd.merge(all_, songs_lives_cross, left_on="曲名", right_on="曲名", how="left")
+    all_song_ = pd.merge(all_song_, counted_live_songs, left_on="曲名", right_on="曲名", how="left")
+
+
